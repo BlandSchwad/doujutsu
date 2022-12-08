836aab9e3cc27a28bd80ef20fd4b09e218dcdbc4
Andy <schwaderer@protonmail.com>
2022-12-08 13:29:28 -0800

    demo build server files

---
+++ server/controllers.js
@@ -0,0 +1,227 @@
+const unzipit = require("unzipit");
+const fsPromises = require("fs").promises;
+
+const {
+  getLibViews,
+  getLibView,
+  getLibraries,
+  getSeries,
+  getBooks,
+  getBookView,
+  deleteLibrary,
+  updateLibrary,
+  deleteSeries,
+  addLibrary,
+  getPageView,
+  getSeriesView,
+} = require("./models.js");
+// const { getSeriesView } = require("./ripsaw");
+var SHA1 = require("crypto-js/sha1");
+var B64U = require("crypto-js/enc-base64url");
+const { hashScanLibrary } = require("./scanner.js");
+
+class FileReader {
+  constructor(filename) {
+    this.fhp = fsPromises.open(filename);
+  }
+  async close() {
+    const fh = await this.fhp;
+    await fh.close();
+  }
+  async getLength() {
+    if (this.length === undefined) {
+      const fh = await this.fhp;
+      const stat = await fh.stat();
+      this.length = stat.size;
+    }
+    return this.length;
+  }
+  async read(offset, length) {
+    const fh = await this.fhp;
+    const data = new Uint8Array(length);
+    await fh.read(data, 0, length, offset);
+    return data;
+  }
+}
+
+module.exports.librariesController = async function (req, res) {
+  try {
+    let result = await getLibViews();
+    res.status(200).send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+module.exports.libraryPostController = async function (req, res) {
+  try {
+    const id = B64U.stringify(SHA1(req.body.path));
+    const name = req.body.name;
+    const file_path = req.body.path;
+    const created_date = Date.now();
+    const last_modified_date = created_date;
+    await addLibrary({ id, name, created_date, last_modified_date, file_path });
+    res.status(201).send(id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+module.exports.libraryDeleteController = async function (req, res) {
+  try {
+    await deleteLibrary(req.params.library_id);
+    res.status(200).send(`Deleted`);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+module.exports.libraryPutController = async function (req, res) {
+  try {
+    let md = Date.now();
+
+    await updateLibrary({
+      library_id: req.params.library_id,
+      name: req.body.name,
+      path: req.body.path,
+      modDate: md,
+    });
+    res.status(200).send(req.body);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+module.exports.libraryController = async function (req, res) {
+  try {
+    let libraryView = await getLibView(req.params.library_id);
+
+    res.status(200).send(libraryView);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+module.exports.libraryScanController = async function (req, res) {
+  try {
+    let test = await hashScanLibrary(req.params.library_id);
+    res.status(200).send(test);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+module.exports.fetchAllController = async function (req, res) {
+  let data = {
+    libraries: [],
+  };
+  const parseLibrary = async function (library) {
+    let series = await getSeries(library.id);
+    let seriesList = await Promise.all(series.map(parseSeries));
+
+    data.books = seriesList.flat();
+    return series;
+  };
+  const parseSeries = async function (series) {
+    let bookList = await getBooks(series.id);
+    return bookList;
+  };
+
+  data.libraries = await getLibraries();
+
+  try {
+    [data.series] = await Promise.all(data.libraries.map(parseLibrary));
+    data.books = await Promise.all(data.series.map(parseSeries));
+    res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+//SERIES CONTROLLERS
+
+module.exports.seriesController = async function (req, res) {
+  try {
+    let seriesView = await getSeriesView(req.params.series_id);
+    // let bookList = await getBooks(req.params.series_id);
+    // let seriesInfo = await getSeriesInfo(req.params.series_id);
+    res.status(200).send({ seriesView });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+module.exports.deleteSeriesController = async function (req, res) {
+  try {
+    await deleteSeries(req.params.series_id);
+
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+module.exports.scanSeriesController = async function (req, res) {
+  try {
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+//BOOK
+module.exports.bookViewController = async function (req, res) {
+  try {
+    let bookView = await getBookView(req.params.book_id);
+    res.status(200).send(bookView);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+//
+
+//GET PAGE
+
+//I- GET Request /book/:bookID/*read*?page=*number*
+//O- Unzipped image file data akin to the doStuff function
+//C- None
+//E- Invalid numbers and book names
+module.exports.pageViewController = (req, res) => {
+  let q = {};
+  q.id = req.params.book_id;
+  q.number = req.query.page;
+  getPageView(q)
+    .then((result) => {
+      const reader = new FileReader(`${result.book_path}/${result.file_name}`);
+      unzipit
+        .unzip(reader)
+        .then(({ entries }) => {
+          entries[result.name]
+            .arrayBuffer()
+            .then((aBuffer) => {
+              let page = new Uint8Array(aBuffer);
+              reader.close();
+              res
+                .writeHead(200, { "Content-Type": result.media_type })
+                .end(page);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+};
---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const fs = require("fs");
+const { waitFor } = require("wait-for-event");
+
+const {
+  seriesController,
+  librariesController,
+  fetchAllController,
+  bookViewController,
+  libraryDeleteController,
+  libraryPostController,
+  libraryPutController,
+  libraryScanController,
+  libraryController,
+  deleteSeriesController,
+  pageViewController,
+} = require("./controllers");
+const path = require("path");
+// const { ocrPostController, ocrPost2 } = require("./ocrController");
+//const manga_ocr = require(`./spawner`);
+const ocr_spawner = require("./ocr_spawner");
+require("dotenv").config();
+let ocr = new ocr_spawner();
+console.log(path.join(__dirname, "..", "dist"));
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "dist")));
+console.log(`Starting Doujutsu Server`);
+app.listen(process.env.SERVER_PORT);
+ocr.start();
+console.log(
+  `Doujutsu Server ready, listening on port ${process.env.SERVER_PORT}`
+);
+
+app.get("/all", fetchAllController);
+
+//Library Routes
+app.get("/libraries", librariesController);
+app.get("/library/scan/:library_id", libraryScanController);
+app.get("/library/:library_id", libraryController);
+app.delete("/library/:library_id", libraryDeleteController);
+app.post("/library", libraryPostController);
+app.put("/library/:library_id", libraryPutController);
+
+//Series
+app.get("/series/:series_id", seriesController);
+app.delete("/series/:series_id", deleteSeriesController);
+//app.get('/series/scan/:series_id', )
+
+// Book
+app.get("/book/:book_id", bookViewController);
+
+//Page
+app.get("/page/:book_id", pageViewController);
+
+//OCR
+app.post(
+  "/ocr",
+  /*ocrPostController*/ async (req, res) => {
+    ocr.translate(req.body);
+    try {
+      await waitFor("translated", ocr.instance, () => {
+        let kanji = fs
+          .readFileSync(process.env.OCR_WRITE_DIR, "utf8")
+          .split("\n");
+        res.status(201).send(kanji[kanji.length - 2]);
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(401).send(err);
+    }
+  }
+);
